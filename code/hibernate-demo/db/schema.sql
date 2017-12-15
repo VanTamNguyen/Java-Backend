2f70c04d5fe42b9b6ed60522706bcb40d91598d2
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2017-12-15 23:34:46 +0700

    Hibernate demo code

---
+++ code/hibernate-demo/db/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS person(
+    id VARCHAR(36) PRIMARY KEY,
+    name TEXT NOT NULL,
+    salary INTEGER NOT NULL
+) CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS bank_account(
+    id VARCHAR(36) PRIMARY KEY,
+    account VARCHAR(36) NOT NULL,
+    balance INTEGER NOT NULL
+) CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS person_bank_account(
+    person_id NOT NULL ,
+    bank_account_id NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES person(id),
+    FOREIGN KEY (bank_account_id) REFERENCES bank_account(id)
+) CHARACTER SET utf8;
